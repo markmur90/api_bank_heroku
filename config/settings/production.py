e6b35d45cee54dab882298608f01289259efcf63
markmur90 <markmur90@proton.me>
2025-04-19 17:28:38 -0500

    Actualizar ALLOWED_HOSTS y ORIGIN en la configuración de producción

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,8 +23,8 @@ load_dotenv(os.path.join(BASE_DIR, 'entorno.env'))
 # Configuración de seguridad
 SECRET_KEY = 'django-insecure-z*_hrh0ssiq1s5t=dqe5_c6@qfb(fhb!+ruqnvu$mxi4$*m0s4'
 DEBUG = False
-ALLOWED_HOSTS = ['api-bank-mt-8baa98a9ca2b.herokuapp.com', '0.0.0.0']
-ORIGIN = 'api-bank-mt-8baa98a9ca2b.herokuapp.com'
+ALLOWED_HOSTS = ['api-bank-heroku-72c443ab11d3.herokuapp.com', '0.0.0.0']
+ORIGIN = 'api-bank-heroku-72c443ab11d3.herokuapp.com'
 # https://api-bank-mt-8baa98a9ca2b.herokuapp.com/ | https://git.heroku.com/api-bank-mt.git
 
 # Installed apps
