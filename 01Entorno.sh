8798a6b8772144a263daf8713e332e44d651a378
markmur90 <markmur90@proton.me>
2025-04-22 23:32:26 -0500

    Mejorar scripts de inicio: actualizar rutas, agregar instalación de dependencias y configurar entorno virtual para la aplicación Django.

---
+++ 01Entorno.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Inicio
+
+python3 -m venv ~/Documentos/Entorno/venvAPI
+source ~/Documentos/Entorno/venvAPI/bin/activate
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
+python3 manage.py collectstatic --noinput
+
+echo "✅ ✅ ✅ Environment setup complete. You can now run your Django application.✅ ✅ ✅ "
