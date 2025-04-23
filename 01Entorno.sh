5781d80e1ba795710ac6ff12b31edf1289087994
markmur90 <markmur90@proton.me>
2025-04-23 00:35:01 -0500

    Cambios realizados el 2025-04-23 00:35:01:

--- 01Entorno.sh
+++ 01Entorno.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Inicio
 
-python3 -m venv ~/Documentos/Entorno/venvAPI
-source ~/Documentos/Entorno/venvAPI/bin/activate
+python3 -m venv $HOME/Documentos/Entorno/venvAPI
+source $HOME/Documentos/Entorno/venvAPI/bin/activate
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
