eca4c0ebb1e594335715d217b54f5cc628f20519
markmur90 <markmur90@proton.me>
2025-04-24 23:35:01 -0500

    Cambios realizados el 2025-04-24 23:35:01:

--- 03Gunicorn.sh
+++ 03Gunicorn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Gunicorn
+
 export DATABASE_URL="postgres://markmur88:Ptf8454Jd55@localhost:5432/mydatabase"
 
 #export DATABASE_URL="postgres://ue2erdhkle4v0h:pa1773a2b68d739e66a794acd529d1b60c016733f35be6884a9f541365d5922cf@ec2-63-33-30-239.eu-west-1.compute.amazonaws.com:5432/d9vb99r9t1m7kt"
@@ -8,5 +9,7 @@ source $HOME/Documentos/Entorno/venvAPI/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
+
 clear
+
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
